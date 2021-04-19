b06ac8b9b49c9b98d17b5da96cb996357f538259
Edmund <ninjataco127@gmail.com>
2021-04-18 23:04:20 -0700

    Initial Commit

---
+++ Project2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project2
+//
+//  Created by Edmund Zhi on 4/18/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
